b006a33a7579d7e62663d4c7e09ec0d87d1f050c
Dmytro <ddorotiuk@gmail.com>
2017-07-31 11:03:22 +0200

    Modified scripts file

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,32 @@
 
 $(function() {
 	var carouselList = $('#carousel ul');
-	setInterval(changeSlide, 3000);
+	//setInterval(changeSlide, 3000);
 	function changeSlide() {
-		carouselList.animate({'marginLeft':-400}, 500, moveFirstSlide);
+		carouselList.animate({'marginLeft':-800}, 500, moveFirstSlide);
 	};
+
 	function moveFirstSlide() {
 		var firstItem = carouselList.find("li:first");
 		var lastItem = carouselList.find("li:last");
 		lastItem.after(firstItem);
+		carouselList.css({marginLeft:-400});
+	};
+	$('#right').click(function(){
+		changeSlide();
+	});
+	
+	function moveLastSlide() {
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+		lastItem.before(firstItem);
 		carouselList.css({marginLeft:0});
 	};
+
+	function changeLeftSlide() {
+		carouselList.animate({'marginLeft':0}, 500, moveLastSlide);
+	};
+	$('#left').click(function(){
+		changeLeftSlide();
+	});
 });
